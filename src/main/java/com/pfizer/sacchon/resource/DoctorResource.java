f11f539068caa7639c8ac4100ca348bf6c243ac0
vasipara4 <billprig@gmail.com>
2020-09-15 03:04:10 +0300

    Initialize DoctorResources

---
+++ src/main/java/com/pfizer/sacchon/resource/DoctorResource.java
@@ -0,0 +1,33 @@
+package com.pfizer.sacchon.resource;
+
+import com.pfizer.sacchon.representation.DoctorRepresentation;
+import com.pfizer.sacchon.representation.NoteRepresentation;
+import com.pfizer.sacchon.representation.PatientRepresentation;
+import javassist.NotFoundException;
+import org.restlet.resource.Delete;
+import org.restlet.resource.Get;
+import org.restlet.resource.Post;
+import org.restlet.resource.Put;
+
+import java.util.List;
+
+public interface DoctorResource {
+
+    @Delete
+    boolean deleteDoctor(DoctorRepresentation doctorReprIn) throws NotFoundException;
+
+    @Get("json")
+    List<PatientRepresentation> findInactivePatients() throws NotFoundException;
+
+    @Get("json")
+    List<PatientRepresentation> findMyPatients(DoctorRepresentation doctorReprIn) throws NotFoundException;
+
+    @Get("json")
+    List patientRecord(PatientRepresentation patientReprIn) throws NotFoundException;
+
+    @Post("json")
+    boolean postNote(NoteRepresentation noteReprIn) throws NotFoundException;
+
+    @Put("json")
+    boolean updateNote(NoteRepresentation noteReprIn) throws NotFoundException;
+}
---
+++ src/main/java/com/pfizer/sacchon/resource/DoctorResourceImpl.java
@@ -0,0 +1,47 @@
+package com.pfizer.sacchon.resource;
+
+import com.pfizer.sacchon.model.Patient;
+import com.pfizer.sacchon.representation.DoctorRepresentation;
+import com.pfizer.sacchon.representation.NoteRepresentation;
+import com.pfizer.sacchon.representation.PatientRepresentation;
+import javassist.NotFoundException;
+import org.restlet.engine.Engine;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+public class DoctorResourceImpl implements DoctorResource {
+
+    public static final Logger LOGGER = Engine.getLogger(DoctorResourceImpl.class);
+
+    @Override
+    public boolean deleteDoctor(DoctorRepresentation doctorReprIn) throws NotFoundException {
+        return false;
+    }
+
+    @Override
+    public List<PatientRepresentation> findInactivePatients() throws NotFoundException {
+        return null;
+    }
+
+    @Override
+    public List patientRecord(PatientRepresentation patientReprIn) throws NotFoundException {
+        return null;
+    }
+
+    @Override
+    public List<PatientRepresentation> findMyPatients(DoctorRepresentation doctorReprIn) throws NotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean postNote(NoteRepresentation noteReprIn) throws NotFoundException {
+
+        return false;
+    }
+
+    @Override
+    public boolean updateNote(NoteRepresentation noteReprIn) throws NotFoundException {
+        return false;
+    }
+}
