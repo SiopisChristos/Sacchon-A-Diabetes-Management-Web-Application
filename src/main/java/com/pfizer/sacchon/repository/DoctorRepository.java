6f213cb98440d20d7ce2a1156bbb202d67edf840
vasipara4 <billprig@gmail.com>
2020-09-17 21:48:57 +0300

    Fix Hibernate queries

--- src/main/java/com/pfizer/sacchon/repository/DoctorRepository.java
+++ src/main/java/com/pfizer/sacchon/repository/DoctorRepository.java
@@ -1,12 +1,18 @@
 package com.pfizer.sacchon.repository;
 
 import com.pfizer.sacchon.exception.BadEntityException;
-import com.pfizer.sacchon.model.Note;
-import com.pfizer.sacchon.model.Patient;
-import com.pfizer.sacchon.model.Doctor;
+import com.pfizer.sacchon.exception.NotAuthorizedException;
+import com.pfizer.sacchon.model.*;
+import com.pfizer.sacchon.representation.CarbRepresentation;
+import com.pfizer.sacchon.representation.GlucoseRepresentation;
+import com.pfizer.sacchon.representation.NoteRepresentation;
+import org.restlet.resource.ServerResource;
+
 import javax.persistence.EntityManager;
-import java.util.List;
-import java.util.Optional;
+import javax.persistence.Query;
+import java.util.*;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class DoctorRepository {
 
@@ -20,7 +26,6 @@ public class DoctorRepository {
     // hasRightToSee method
 
 
-
     /**
      * Find the patients for a specific doctor_id
      *
@@ -28,11 +33,22 @@ public class DoctorRepository {
      * @return A list of his patients
      */
     public List<Patient> findMyPatients(long doctor_id) {
-        List results = entityManager.createQuery(
-                "SELECT * from Patient WHERE doctor_id = :doctor_id").setParameter("doctor_id", doctor_id)
+        List<Patient> results = entityManager.createQuery(
+                "from Patient where :id = doctor_id").setParameter("id", doctor_id)
                 .getResultList();
         return results;
     }
+
+
+    public List<Patient> findFreePatients() {
+        List<Patient> results = entityManager.createQuery(
+                "from Patient where doctor_id is null")
+                .getResultList();
+        return results;
+    }
+
+
+
 
     /**
      * Checks if a patient is under supervision of a specific doctor
@@ -44,61 +60,40 @@ public class DoctorRepository {
     public boolean isYourPatient(long patient_id, long doctor_id) {
 
         List<Patient> results = findMyPatients(doctor_id);
-        if (results.isEmpty()) return false;
-        else if ((long) results.get(0).getId() == patient_id) return true;
+        if (results.isEmpty())
+            return false;
+        else if (results.get(0).getId() == patient_id)
+            return true;
+        else
+            return false;
+    }
+
+
+    public boolean isFreePatient(long patient_id) {
+        Patient patient = (Patient) entityManager.createQuery(
+                "from Patient where doctor_id is null and patient_id = :id").
+                setParameter("id",patient_id).getSingleResult();
+        if (!patient.equals(null))
+            return true;
         return false;
     }
 
 
-    /**
-     * Returns all the patient data (Carbs & Glucose measurements) and their consultations in
-     * a List structure which contains three Lists.
-     * The list structure is: {Carbs, Glucose, Notes}
-     *
-     * @param patient_id the integer id of a specific patient
-     * @return patient data and consultations
-     */
-    public List[] patientRecord(long patient_id) {
-        List carb = entityManager.createQuery(
-                "SELECT * from Carb c  WHERE :p.id = c.patient_id")
-                .setParameter("p.id", patient_id).getResultList();
-        List glucose = entityManager.createQuery(
-                "SELECT * from Glucose g  WHERE :p.id = g.patient_id")
-                .setParameter("p.id", patient_id).getResultList();
-        List consultations = entityManager.createQuery(
-                "SELECT * from Note n  WHERE :p.id = n.patient_id").getResultList();
-
-        return new List[]{carb, glucose, consultations};
+    public void doctorAccessPatientData(Long patient_id, Doctor doctor) throws NotAuthorizedException {
+        if (!isYourPatient(patient_id, doctor.getId()) || !isFreePatient(patient_id))
+            throw new NotAuthorizedException("Not Authorized Doctor");
     }
 
+
     /**
-     * Select all the inactive Patients
+     * Select all the inactive Patients from Db
      *
      * @return a List which contains all their personal data
      */
     public List<Patient> findInactivePatients() {
         return entityManager.createQuery(
-                "SELECT * from Patient WHERE doctor_id IS NULL")
+                "from Patient p where p.doctor_id IS NULL")
                 .getResultList();
-    }
-
-
-    /**
-     * Saving a new note to the Db
-     *
-     * @param note The note to be stored
-     * @return True if saving has been completed, else false
-     */
-    public boolean saveNote(Note note) {
-        try {
-            entityManager.getTransaction().begin();
-            entityManager.persist(note);
-            entityManager.getTransaction().commit();
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
     }
 
 
@@ -108,13 +103,13 @@ public class DoctorRepository {
      * @param id The doctor to be deleted
      * @return True if deleting has been completed, else false
      */
-    public boolean removeDoctor(Long id){
-        Optional<Doctor> thedoctor = findDoctorById(id);
-        if (thedoctor.isPresent()){
-            Doctor d = thedoctor.get();
-            try{
+    public boolean removeDoctor(Long id) {
+        Optional<Doctor> oDoctor = findDoctorById(id);
+        if (oDoctor.isPresent()) {
+            Doctor doctor = oDoctor.get();
+            try {
                 entityManager.getTransaction().begin();
-                entityManager.remove(d);
+                entityManager.remove(doctor);
                 entityManager.getTransaction().commit();
                 return true;
             } catch (Exception e) {
@@ -127,56 +122,37 @@ public class DoctorRepository {
 
 
     /**
-     * Updates an existing note in Db
-     *
-     * @param note the updated note
-     * @return True if updating has been completed, else false
+     * @param username
+     * @return
      */
-    public boolean updateNote(Note note) {
-        Note in = entityManager.find(Note.class, note.getId());
-        in.setMessage(note.getMessage());
-        in.setDate(note.getDate());
-        in.setDoctor(note.getDoctor());
-        in.setPatient(note.getPatient());
-        try {
-            entityManager.getTransaction().begin();
-            entityManager.persist(in);
-            entityManager.getTransaction().commit();
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
+    public Optional<Doctor> findDoctorByUsername(String username) {
+        Doctor doctor = (Doctor) entityManager.createQuery(
+                "from Doctor d where :username = d.username").setParameter("username", username).getSingleResult();
+        return doctor != null ? Optional.of(doctor) : Optional.empty();
     }
 
+
     /**
-     * find doctor by
+     * find doctor by id
+     *
      * @param id
      * @return
      */
     public Optional<Doctor> findDoctorById(long id) {
-            Doctor doctor = entityManager.find(Doctor.class, id);
-            return doctor != null ? Optional.of(doctor) : Optional.empty();
-        }
+        Doctor doctor = entityManager.find(Doctor.class, id);
+        return doctor != null ? Optional.of(doctor) : Optional.empty();
+    }
 
     /**
      * find a Patient record by ID
+     *
      * @param patient_id
-     * @return  Optional of Patient or empty
+     * @return Optional of Patient or empty
      */
     public Optional<Patient> findPatientById(long patient_id) {
-        Patient patient =  entityManager.find(Patient.class, patient_id);
+        Patient patient = entityManager.find(Patient.class, patient_id);
         return patient != null ? Optional.of(patient) : Optional.empty();
     }
 
-    public Optional<Note> findNoteById(long note_id) {
-        Note note =  entityManager.find(Note.class, note_id);
-        return note != null ? Optional.of(note) : Optional.empty();
-    }
-
-    public  <T> Optional<T>  findEntityById(T entity, long entity_id){
-        entity = (T) entityManager.find(entity.getClass(), entity_id);
-        return  (entity!= null) ? Optional.of(entity) : Optional.empty();
-    }
 }
 
