113fc1c4286f2653c03f5a7dda884fade2596bd6
vasipara4 <billprig@gmail.com>
2020-09-17 21:42:32 +0300

    Add Note Integrity

--- src/main/java/com/pfizer/sacchon/resource/util/ResourceValidator.java
+++ src/main/java/com/pfizer/sacchon/resource/util/ResourceValidator.java
@@ -1,38 +1,21 @@
 package com.pfizer.sacchon.resource.util;
 
 import com.pfizer.sacchon.exception.BadEntityException;
-import org.restlet.Request;
-import org.restlet.resource.ServerResource;
-import org.restlet.security.User;
+import com.pfizer.sacchon.exception.NotAuthorizedException;
+import com.pfizer.sacchon.model.Note;
+
 
 import java.util.Optional;
 import java.util.logging.Logger;
 
 public class ResourceValidator {
 
-
-    public static boolean userAuthorization(String username) {
-        User currentSystemUsername;
-        Request request = Request.getCurrent();
-        currentSystemUsername = request.getClientInfo().getUser();
-        System.out.println("USer" + currentSystemUsername);
-        if (currentSystemUsername.toString().equals(username))
-            return true;
-
-        return false;
+    public static void checkNoteIntegrity(Note oldNote, Note noteIn) throws BadEntityException {
+        if (!oldNote.getDoctor().equals(noteIn.getDoctor()))
+            throw new BadEntityException("Can't change doctor_id in your Note");
+        if (!oldNote.getPatient().equals(noteIn.getPatient()))
+            throw new BadEntityException("Can't change patient_id in your Note");
     }
 
-
-    public static <T> T getFromOptionalEntityById(Optional<T> entity, ServerResource serverResource, Logger LOGGER) throws BadEntityException {
-        Class<?> type = null;
-        serverResource.setExisting(entity.isPresent());
-        if (!serverResource.isExisting()) {
-            type = entity.get().getClass();
-            System.out.println(type.toString() + " id does not exist");
-            LOGGER.config(type.toString() + " id does not exist");
-            throw new BadEntityException(type.toString() + " id does not exist");
-        }
-        return entity.get();
-    }
 
 }
