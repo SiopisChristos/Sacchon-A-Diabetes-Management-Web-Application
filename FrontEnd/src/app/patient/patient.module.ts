4ac828dc4385bbc71262e6cb49516d5f43feb355
George Stathis <georgestathis1996@yahoo.gr>
2020-09-27 20:32:06 +0300

    add patientDelete

--- FrontEnd/src/app/patient/patient.module.ts
+++ FrontEnd/src/app/patient/patient.module.ts
@@ -7,21 +7,22 @@ import { Routes } from '@angular/router';
 import { CarbEntryComponent } from './carb/carb-entry/carb-entry.component';
 import { CarbModule } from './carb/carb.module';
 import { GlucoseModule } from './glucose/glucose.module';
+import { PatientDeleteComponent } from './patient-delete/patient-delete.component';
 
 const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [PatientComponent, PatientRegisterComponent],
+  declarations: [PatientComponent, PatientRegisterComponent, PatientDeleteComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     CarbModule,
     GlucoseModule
   ],
-  exports:[
+  exports: [
     PatientRegisterComponent,
-    PatientComponent
+    PatientComponent, PatientDeleteComponent
   ]
 })
 export class PatientModule { }
