02df49c4ac4ac7b5e2be777ea571bbbe83104224
Irene <irenecovers@gmail.com>
2020-09-28 19:27:47 +0300

    fix note interface

--- FrontEnd/src/app/doctor/note/note.ts
+++ FrontEnd/src/app/doctor/note/note.ts
@@ -2,4 +2,9 @@ export interface Note {
     id: number;
     message: string;
     date: Date;
+    patientLastname: string;
+    doctorLastname: string;
+    patient_id: number;
+    doctor_id: number;
+    seen: boolean;  
 }
