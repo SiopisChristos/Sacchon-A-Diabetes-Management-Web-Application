78c2554065fd9a5b7ef0837bae00b609788a0901
vasipara4 <billprig@gmail.com>
2020-09-30 02:02:00 +0300

    Add doctorID to Representation

--- FrontEnd/src/app/doctor/doctor.ts
+++ FrontEnd/src/app/doctor/doctor.ts
@@ -1,8 +1,9 @@
 export interface Doctor {
+    id:number,
     username:string,
     lastName:string,
     firstName:string,
     phoneNumber: string,
     dateOfBirth: Date,
-    specialty: string
+    specialty: string,
 }
